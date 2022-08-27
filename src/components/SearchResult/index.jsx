2840754e21f5855daaf8e67b665ffa76b3046906
BorrachaFox <daviantoniosantos@outlook.com>
2022-08-27 20:13:33 -0300

    Change to Vite

--- src/components/SearchResult/index.jsx
+++ src/components/SearchResult/index.jsx
@@ -1,60 +1,27 @@
-import React from "react";
-import styled from "styled-components";
-
-const Content = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-    width: 100%; 
-
-    margin: 40px auto;
-    border-left: 1px solid #000;
-`;
-
-const Item = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center; 
-    justify-content: center;
-
-    height: 80px;
-    border-right: 1px solid #000;
-    font-family: ${(props) => props.theme.fonts.regular};
-    text-align: center;
-
-    p {
-       font-size: 18px; 
-       font-weight: 500;
-       color: #4a4a4a;
-    }
-    
-    h2 {
-        font-weight: 500;
-        font-size: 24px;
-        margin-top: 6px;
-    }
-`;
+import React from "react"
+import "./style.scss"
 
 const SearchResult = (props) => {
-    return (
-        <Content>
-            <Item>
-                <p>Cidade, Estado:</p>
-                <h2>SALVADOR-BA</h2>
-            </Item>
-            <Item>
-                <p>Ip/Domain:</p>
-                <h2>123.09.84.1</h2>
-            </Item>
-            <Item>
-                <p>Continente:</p>
-                <h2>AMERICA DO SUL</h2>
-            </Item>
-            <Item>
-                <p>País:</p>
-                <h2>BRASIL</h2>
-            </Item>
-        </Content>
-    )
+  return (
+    <div className="content">
+      <div className="item">
+        <p>Cidade, Estado:</p>
+        <h2>SALVADOR-BA</h2>
+      </div>
+      <div className="item">
+        <p>Ip/Domain:</p>
+        <h2>123.09.84.1</h2>
+      </div>
+      <div className="item">
+        <p>Continente:</p>
+        <h2>AMERICA DO SUL</h2>
+      </div>
+      <div className="item">
+        <p>País:</p>
+        <h2>BRASIL</h2>
+      </div>
+    </div>
+  )
 }
 
-export default SearchResult;
+export default SearchResult
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,50 +1,40 @@
-import React, { useState } from "react";
-import { createGlobalStyle } from "styled-components";
+import React, { useState } from "react"
 
-import { Container, Header, Logo, Ip, MainText, Title, Description, MainSection, IpSearch, Map, Footer } from './styles';
-import { SearchResult } from '../../components';
+import { SearchResult } from '../../components'
 
 import SvgWorld from '../../assets/img/connected-world.svg'
 import MapImage from '../../assets/img/map.png'
 
-const GlobalStyle = createGlobalStyle`
-    * {
-        box-sizing: border-box;
-        font-family: ${(props) => props.theme.fonts.regular};
-    }
-`;
+import "./style.scss"
 
 const Home = () => {
-    return (
-        <>
-            <GlobalStyle/>
-            <Container>
-                <Header>
-                    <Logo>IpCats</Logo>
-                    <Ip>IP: 123.09.84.1</Ip>
-                </Header>
-                <MainSection>
-                    <MainText>
-                        <Title>SITE PARA BUSCAR IP</Title>
-                        <Description>Criado com o intuito de buscar por IP de maquinas e dominios de sites</Description>
-                    </MainText>                
-                    <img src={SvgWorld} alt="Connected world"/>
-                </MainSection>
-                <IpSearch>
-                    <h2>DIGITE O IP/DOMINIO DESEJADO:</h2>
-                    <div>
-                        <input type="text" id="SearchIp" name="SearchIp" placeholder="IP/DOMAIN"/>
-                        <button>Search</button>
-                    </div>
-                </IpSearch>
-                <SearchResult/>
-                <Map>
-                    <img src={MapImage} alt="Location map" width="" />
-                </Map>
-                <Footer/>
-            </Container>
-        </>
-    )
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="logo">IpCats</div>
+        <div className="ip">IP: 123.09.84.1</div>
+      </div>
+      <div className="mainSection">
+        <div className="mainText">
+          <div className="title">SITE PARA BUSCAR IP</div>
+          <div className="description">Criado com o intuito de buscar por IP de maquinas e dominios de sites</div>
+        </div>                
+        <img src={SvgWorld} alt="Connected world"/>
+      </div>
+      <div className="ipSearch">
+        <h2>DIGITE O IP/DOMINIO DESEJADO:</h2>
+        <div>
+          <input type="text" id="SearchIp" name="SearchIp" placeholder="IP/DOMAIN"/>
+          <button>Search</button>
+        </div>
+      </div>
+      <SearchResult/>
+      <div className="map">
+        <img src={MapImage} alt="Location map" width="" />
+      </div>
+      <div className="footer"/>
+    </div>
+  )
 }
 
-export default Home;
+export default Home
