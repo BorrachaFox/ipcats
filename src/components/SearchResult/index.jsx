5a69513c1592e31806bf2430ad4b3460b741cdda
BorrachaFox <daviantoniosantos@outlook.com>
2022-08-28 00:42:51 -0300

    Ip handle

--- src/components/SearchResult/index.jsx
+++ src/components/SearchResult/index.jsx
@@ -1,24 +1,31 @@
-import React from "react"
+import React, { useState } from "react"
 import "./style.scss"
 
-const SearchResult = (props) => {
+const SearchResult = ({locationData}) => {
+
   return (
     <div className="content">
       <div className="item">
         <p>Cidade, Estado:</p>
-        <h2>SALVADOR-BA</h2>
+        <h2>
+          {!locationData.region_code ? (
+            locationData.city
+          ) : (
+            locationData.city+"-"+locationData.region_code
+          )}
+        </h2>
       </div>
       <div className="item">
         <p>Ip/Domain:</p>
-        <h2>123.09.84.1</h2>
+        <h2>{locationData.ip}</h2>
       </div>
       <div className="item">
         <p>Continente:</p>
-        <h2>AMERICA DO SUL</h2>
+        <h2>{locationData.continent_code}</h2>
       </div>
       <div className="item">
         <p>País:</p>
-        <h2>BRASIL</h2>
+        <h2>{locationData.country_name}</h2>
       </div>
     </div>
   )
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,34 +1,71 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 import { SearchResult } from '../../components'
+import { api } from "../../services/api"
 
 import SvgWorld from '../../assets/img/connected-world.svg'
 import MapImage from '../../assets/img/map.png'
 
 import "./style.scss"
+import axios from "axios"
 
 const Home = () => {
+  const [userIp, setUserIp] = useState('');
+  const [location, setLocation] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  async function getIpAdress(ipAdress) {
+    const res = await api.get(`${ipAdress}`+'/json/')
+    const data = (res.data)
+    setLocation(data)
+    
+    if(!ipAdress) setUserIp(data.ip)
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    if(inputValue) getIpAdress(inputValue);
+    setInputValue('');
+  };
+
+  useEffect(() => {
+    getIpAdress('')
+  }, [])
+  
+  useEffect(() => {
+    console.log(userIp)
+  }, [userIp]);
+
   return (
     <div className="container">
       <div className="header">
         <div className="logo">IpCats</div>
-        <div className="ip">IP: 123.09.84.1</div>
+        <div className="ip">IP: {userIp}</div>
       </div>
       <div className="mainSection">
         <div className="mainText">
           <div className="title">SITE PARA BUSCAR IP</div>
-          <div className="description">Criado com o intuito de buscar por IP de maquinas e dominios de sites</div>
+          <div className="description">
+            Criado com o intuito de buscar por IP de maquinas e dominios de sites
+          </div>
         </div>                
         <img src={SvgWorld} alt="Connected world"/>
       </div>
       <div className="ipSearch">
         <h2>DIGITE O IP/DOMINIO DESEJADO:</h2>
-        <div>
-          <input type="text" id="SearchIp" name="SearchIp" placeholder="IP/DOMAIN"/>
-          <button>Search</button>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="text" 
+            id="SearchIp" 
+            name="SearchIp" 
+            placeholder="IP/DOMAIN"
+            onChange={event => setInputValue(event.target.value)} 
+            value={inputValue}
+          />
+          <button type="submit">Search</button>
+        </form>
       </div>
-      <SearchResult/>
+      <SearchResult locationData={location}/>
       <div className="map">
         <img src={MapImage} alt="Location map" width="" />
       </div>
